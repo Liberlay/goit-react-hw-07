35a59410317fd5b808b72a5d2aa96778f8d61599
Danyil <liberlay555@gmail.com>
2025-04-06 23:56:15 +0300

    Fix ContactList component

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,20 +1,16 @@
 import { useSelector } from 'react-redux'
-import { selectNameFilter } from '../../redux/filtersSlice'
-import { selectContacts } from '../../redux/contactsSlice'
+import { selectFilteredContacts } from '../../redux/contactsSlice'
 
 import Contact from '../Contact/Contact'
 
 import styles from './ContactList.module.scss'
 
 export default function ContactList() {
-  const contacts = useSelector(selectContacts)
-  const filter = useSelector(selectNameFilter)
-
-  const visibleContacts = contacts.filter((contact) => contact.name.toLowerCase().includes(filter.toLowerCase()))
+  const contacts = useSelector(selectFilteredContacts)
 
   return (
     <div className={styles.container}>
-      {visibleContacts.map((contact) => (
+      {contacts.map((contact) => (
         <Contact key={contact.id} contact={contact} />
       ))}
     </div>
